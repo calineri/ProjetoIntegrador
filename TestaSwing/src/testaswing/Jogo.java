dc15fb4886820eafdb70403fc147d5f12cc3dc82
celso.alineri <celso.alineri@CAS7511782W1021.senacsp.edu.br>
2019-04-11 22:03:28 -0300

    Mais metodos de desenho e modos de escrever textos na tela

--- TestaSwing/src/testaswing/Jogo.java
+++ TestaSwing/src/testaswing/Jogo.java
@@ -7,6 +7,7 @@ package testaswing;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import static java.lang.Math.floor;
 import javax.swing.JFrame;
@@ -27,11 +28,31 @@ public class Jogo extends JFrame{
     
     @Override
     public void paint(Graphics g){
+        /*
         // limpa a janela
         Dimension d = this.getSize();
         g.setColor(Color.BLUE);
         g.fillOval((int) floor(d.getWidth()/2)-50,(int) floor(d.getHeight()/2)-25,50,50);
+        */
         
+        g.setColor(Color.BLACK);
+        g.fillRect(750, 550, 50, 50);
+        
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 500, 500);
+        
+        g.setColor(Color.DARK_GRAY);
+        g.fillOval(150,200,200,40);
+        
+        g.setColor(Color.RED);
+        g.fillOval(100,80,150,150);
+        
+        g.setColor(Color.PINK);
+        g.fillOval(120,105,30,30);
+        
+        g.setColor(Color.BLUE);
+        g.setFont(new Font("Arial Bold", Font.PLAIN, 36));
+        g.drawString("Projeto Integrador III", 30,450);
         
     }
     
